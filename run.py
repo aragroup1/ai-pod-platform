461fd894ea0fe03a415babc0a93c872fb6416920
aragroup1 <89531812+aragroup1@users.noreply.github.com>
2026-01-09 22:25:14 +0000

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-import uvicorn
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    print(f"🚀 Starting server on port {port}...")
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, log_level="info")
